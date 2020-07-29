403d860e86d355b848688ff7ebc60b41b9f0b06f
Saurabh Dobhal <Saurabh.Dobhal13@gmail.com>
2020-07-30 00:39:19 +0530

    adding new files

---
+++ src/test/java/com/dashboard/hystrixdashboard/HystrixDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dashboard.hystrixdashboard;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class HystrixDashboardApplicationTests {
+	public HystrixDashboardApplicationTests(){}
+	@Test
+	public void contextLoads() {
+	}
+
+}
